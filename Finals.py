8bf6aaddbdeb2ac42d931b86655531a6fee7890f
Itsangeline <jhelaimagada@gmail.com>
2024-12-18 23:54:05 +0800

    Add files via upload

---
+++ Finals.py
@@ -0,0 +1,11 @@
+list = ["option 1 99php, option 2 129php, option 3 160php"]
+
+def menu():
+    print(list)
+    print("option 1[1] : Rice and Chicken only")
+    print("option 2[2] : Chicken and spaghetti")
+    print("option 3[3] : Rice, Chicken with Fries")
+    print("option 4[4] : Exit")
+
+def option1():
+    print("You order Rice and chicken only")
